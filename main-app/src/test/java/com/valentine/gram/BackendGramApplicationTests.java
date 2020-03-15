c83b4376f97b227974dee1b62f0fc4f171d062af
valentine-ezugu <valentineezugu@yahoo.com>
2020-03-15 22:31:06 +0100

    A few changes for postgres:heroku, configuration

--- main-app/src/test/java/com/valentine/gram/BackendGramApplicationTests.java
+++ main-app/src/test/java/com/valentine/gram/BackendGramApplicationTests.java
@@ -1,14 +1,12 @@
 package com.valentine.gram;
 
 import com.valentine.gram.controller.PostController;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.context.junit4.SpringRunner; 
 
 
 @RunWith(SpringRunner.class)
