53a46d4bca8522e3279165cc70c2c337a7f9272b
valentine-ezugu <valentineezugu@yahoo.com>
2020-04-04 11:49:44 +0200

    procfile

--- main-app/src/main/java/com/valentine/gram/MainApp.java
+++ main-app/src/main/java/com/valentine/gram/MainApp.java
@@ -3,7 +3,9 @@ package com.valentine.gram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,10 +16,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories("com.valentine.dao")
 @EnableTransactionManagement
 @EnableConfigurationProperties
-public class MainApp {
+public class MainApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApp.class);
+    }
 }
